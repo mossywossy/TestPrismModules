2603b4877cdf464783e25479e50dbeb6d18aa0d0
tomas_winston <tomas.winston@gmail.com>
2020-10-28 18:57:32 +0000

    Updated Prism.Forms and Prism.Forms.Regions

--- TestPrismModules/ViewModels/MainPageViewModel.cs
+++ TestPrismModules/ViewModels/MainPageViewModel.cs
@@ -2,6 +2,7 @@
 using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Regions;
+using Prism.Regions.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,14 @@ namespace TestPrismModules.ViewModels
 {
     public class MainPageViewModel : ViewModelBase, INavigatedAware
     {
+        private DelegateCommand loadModuleCommand;
+        public DelegateCommand LoadModuleCommand
+        {
+            get { return loadModuleCommand; }
+            set { SetProperty(ref loadModuleCommand, value); }
+        }
+
+
         private IRegionManager _regionManager;
 
         public MainPageViewModel(INavigationService navigationService, IRegionManager regionManager)
@@ -18,15 +27,26 @@ namespace TestPrismModules.ViewModels
         {
             _regionManager = regionManager;
             Title = "Main Page";
+
+            LoadModuleCommand = new DelegateCommand(ExecuteLoadModuleCommand);
+        }
+
+        private void ExecuteLoadModuleCommand()
+        {
+            _regionManager.RequestNavigate("FlexRegion", "ViewA", NavigationCallback, null);
+        }
+
+        private void NavigationCallback(IRegionNavigationResult result)
+        {
+
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
-            // In WPF I would have been used to RegisterViewWithRegion but seeing as that method doesn't seem to be available 
-            // in Prism.Forms, this was my work-around i.e. to call RequestNavigate to the Module's View upon Navigating to the 
-            // Page that hosts the Region.
+            //////////// In WPF I would have been used to RegisterViewWithRegion but seeing as that method doesn't seem to be available 
+            //////////// in Prism.Forms, this was my work-around i.e. to call RequestNavigate to the Module's View upon Navigating to the 
+            //////////// Page that hosts the Region.
 
-            _regionManager.RequestNavigate("FlexRegion", "ViewA");
         }
     }
 }
